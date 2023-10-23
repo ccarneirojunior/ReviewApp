4a18e575151251d1bc78330c023cf5b918e9bccf
Francisco Carneiro <ccarneirojunior@gmail.com>
2023-10-23 15:46:52 +0300

    changes
    .

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,10 +1,9 @@
 ﻿namespace ReviewApp.Models
 {
-    public class Account
+    public class Category
     {
         public int Id { get; set; }
         public string Name { get; set; }
 
-        public string BirthDate { get; set; }
     }
 }
